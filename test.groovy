8427dbfe202c61f790def192e88b5b6d1bc2f9f2
vanko1987 <80322141+vanko1987@users.noreply.github.com>
2021-03-27 02:27:26 +0000

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -10,8 +10,8 @@ pipeline {
                 script{
                 properties([ 
     parameters([
-        [$class: 'org.biouno.unochoice.ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Select a choice', filterLength: 1, filterable: false, name: 'choice1', script: [$class: 'org.biouno.unochoice.model.GroovyScript', secureFallbackScript: [sandbox: true, script: 'return ["ERROR"]'], secureScript: [sandbox: true, script: 'return[\'aaa\',\'bbb\']']]], 
-        [$class: 'org.biouno.unochoice.CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Active Choices Reactive parameter', filterLength: 1, filterable: false, name: 'choice2', randomName: 'choice-parameter-7601237141171', referencedParameters: 'choice1', script: [$class: 'org.biouno.unochoice.model.GroovyScript', secureFallbackScript: [sandbox: true, script: 'return ["ERROR"]'], secureScript: [sandbox: true, script: 'if(choice1.equals("aaa")){return [\'a\', \'b\']} else {return [\'aaaaaa\',\'fffffff\']}']]]
+        [$class: 'org.biouno.unochoice.ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Select a choice', filterLength: 1, filterable: false, name: 'choice1', script: [$class: 'org.biouno.unochoice.model.GroovyScript', fallbackScript: [sandbox: true, script: 'return ["ERROR"]'], script: [sandbox: true, script: 'return[\'aaa\',\'bbb\']']]], 
+        [$class: 'org.biouno.unochoice.CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Active Choices Reactive parameter', filterLength: 1, filterable: false, name: 'choice2', randomName: 'choice-parameter-7601237141171', referencedParameters: 'choice1', script: [$class: 'org.biouno.unochoice.model.GroovyScript', fallbackScript: [sandbox: true, script: 'return ["ERROR"]'], script: [sandbox: true, script: 'if(choice1.equals("aaa")){return [\'a\', \'b\']} else {return [\'aaaaaa\',\'fffffff\']}']]]
     ])
 ])
             }
